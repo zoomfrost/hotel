bb24f4cc85471f67f405aca0c420d27187ede05d
antonR <zoomfrost@gmail.com>
2024-01-25 23:22:06 +0300

    navbar is dona

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      Footer
+    </>
+  );
+};
+
+export default layout;

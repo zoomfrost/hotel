4c8f887d344a4a3f4850096b78b26b187a84eb4c
antonR <zoomfrost@gmail.com>
2024-01-27 15:23:55 +0300

    stay in touch html css

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -7,6 +7,8 @@ import isaaks from "@/public/isaaks.jpeg";
 import bridge from "@/public/palace-bridge.jpeg";
 import Facilities from "@/components/Facilities";
 import Description from "@/components/Description";
+import { Button } from "@/components/ui/button";
+import bookHotel from "@/public/hotel4.jpg";
 
 const Home = () => {
   const photoArr = [kazan, isaaks, bridge];
@@ -34,6 +36,42 @@ const Home = () => {
         ))}
       </div>
       <Facilities />
+      <div className="grid grid-cols-2 p-4 mt-24 gap-x-44 justify-items-center">
+        <div className="flex flex-col gap-y-8">
+          <h4 className="text-5xl">Stay in touch</h4>
+          <p>Saint-Petersburg, Baskov, 13-15</p>
+          <div className="">
+            <Link
+              className='after:content-["\260e"] after:ml-2'
+              href="tel:+79129112937"
+            >
+              +7912012012
+            </Link>
+            <br />
+            <Link href="mailto:baskov@gmail.com">baskov@gmail.com</Link>
+          </div>
+          <ul className="flex flex-row items-center justify-start gap-x-5">
+            <li>WhatsUp</li>
+            <li>Telegram</li>
+            <li>Instagram</li>
+          </ul>
+        </div>
+        <div className="w-80">
+          <Image className="object-cover" src={bookHotel} alt="room" />
+          <div className="flex w-full">
+            <Button className="w-3/5 h-24 bg-gray-600 hover:bg-gray-500 rounded-none">
+              Book
+            </Button>
+            <div className="bg-amber-100 w-2/5 flex-center">
+              <span>
+                From <br />
+                &nbsp;&nbsp;<b>2400</b>
+                <br /> &nbsp;&nbsp;&nbsp;&nbsp;per night
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- components/Description.tsx
+++ components/Description.tsx
@@ -10,7 +10,7 @@ const Description = ({ photos }: IDescriptionDataProps) => {
   return (
     <>
       {photos.map((item, i) => (
-        <div className="grid grid-cols-2 max-sm:grid-cols-1 justify-items-center max-sm:px-6 max-sm:gap-y-6 gap-x-10 mb-36 px-4">
+        <div className="grid grid-cols-2 max-sm:grid-cols-1 justify-items-center max-sm:px-6 max-sm:gap-y-6 gap-x-10 mb-36 px-6">
           <div className="grid auto-rows-auto max-sm:text-center max-md:text-sm text-md gap-y-4 items-center">
             <h2 className=" heading2 max-md:heading3">The Cabins</h2>
             <p className="">{item.text}</p>

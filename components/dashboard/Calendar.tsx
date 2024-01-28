90f121909d3a18550f96955054e64a45ffca1021
antonR <zoomfrost@gmail.com>
2024-01-28 17:36:15 +0300

    add right calendar to dashboeard

--- components/dashboard/Calendar.tsx
+++ components/dashboard/Calendar.tsx
@@ -1,19 +1,38 @@
 "use client";
 
 import { useState } from "react";
-import { DayPicker } from "react-day-picker";
+import { Calendar } from "../ui/calendar";
+import { DateRange } from "react-day-picker";
+import { addDays, format } from "date-fns";
 
 export default function CalendarDate() {
-  const [date, setDate] = useState<Date | undefined>(new Date());
+  const pastMonth = new Date();
+  const defaultSelected: DateRange = {
+    from: pastMonth,
+    to: addDays(pastMonth, 4),
+  };
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+
+  let footer = <p>Please pick the first day.</p>;
+  if (range?.from) {
+    if (!range.to) {
+      footer = <p>{format(range.from, "PPP")}</p>;
+    } else if (range.to) {
+      footer = (
+        <p>
+          {format(range.from, "PPP")}–{format(range.to, "PPP")}
+        </p>
+      );
+    }
+  }
 
   return (
-    <>
-      <DayPicker
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border shadow"
-      />
-    </>
+    <Calendar
+      mode="range"
+      defaultMonth={pastMonth}
+      selected={range}
+      footer={footer}
+      onSelect={setRange}
+    />
   );
 }

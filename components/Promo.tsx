aa6b5899b4261702aaa5401eac9edc33b3befb38
antonR <zoomfrost@gmail.com>
2024-02-08 18:24:11 +0300

    image test

--- components/Promo.tsx
+++ components/Promo.tsx
@@ -17,6 +17,7 @@ const Promo = ({ title, isHomePage }: IPromoProps) => {
           priority
           fill={true}
           sizes="100vw"
+          quality={80}
           placeholder="blur"
           className=" h-full w-fulls brightness-50 object-cover"
           src={promo}
